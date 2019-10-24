fe95c19e4a2fbccfb71c3b6a68f857fc43eed2e8
huxc <455908283@qq.com>
2019-10-24 16:36:21 +0800

    velocity

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 import qs from 'qs'
+import Velocity from 'velocity-animate'
 
 import mixin from 'js/mixin'
 
@@ -14,7 +15,8 @@ new Vue({
     el: '.container',
     data: {
         searchList: null,
-        keyword
+        keyword,
+        isShow: false,
     },
     created() {
         this.getSearchList()
@@ -24,6 +26,16 @@ new Vue({
             axios.get(url.searchList, { keyword, id }).then(res => {
                 this.searchList = res.data.lists
             })
+        },
+        move() {
+            if (document.documentElement.scrollTop > 100) {
+                this.isShow = true
+            } else {
+                this.isShow = false
+            }
+        },
+        toTop() {
+            Velocity(document.body, 'scroll', { duration: 400 })
         }
     },
     mixins: [mixin]
