0624ffa97a2882a0e747899dae23ecc6c377c53f
huxc <455908283@qq.com>
2019-11-06 02:21:24 +0800

    member

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,20 @@
-import './member_base.css'
-import './member.css'
+// 1.使用vue-router
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+let routes = [{
+    path: '/',
+    component: require('./components/member.vue')
+}]
+
+// 2.创建router实例
+let router = new Router({
+    routes
+  })
+
+// 根组件的注入
+new Vue({
+    el: '#app',
+    router
+  })
