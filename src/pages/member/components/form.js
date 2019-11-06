007987e0af44f19e4ccaccfe750c8f2e018a5ceb
huxc <455908283@qq.com>
2019-11-07 02:34:05 +0800

    provinceValue(val)

--- src/pages/member/components/form.js
+++ src/pages/member/components/form.js
@@ -10,12 +10,26 @@ export default {
             id: '',
             type: '',
             instance: '',
-            addressData: require('js/address.json')
+            addressData: require('js/address.json'),
+            cityList: null,
+            districtList: null,
         }
     },
-    created(){
+    created() {
         let query = this.$route.query
         this.type = query.type
         this.instance = query.instance
+    },
+    watch: {
+        provinceValue(val) {
+            if (val === -1) return
+            let list = this.addressData.list
+            let index = list.findIndex(item => {
+                return item.value === val
+            })
+            this.cityList = list[index].children
+            this.cityValue = -1
+            this.districtValue = -1
+        }
     }
 }
