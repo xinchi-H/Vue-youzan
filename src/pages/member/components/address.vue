fed9fa0130c4aacaa3b58edbab03fcf7decc8567
huxc <455908283@qq.com>
2019-11-06 18:04:04 +0800

    form

---
+++ src/pages/member/components/address.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    地址管理
+  </div>
+</template>
+
+<style scoped>
+@import './address_base.css';
+@import './address.css';
+</style>
--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -4,17 +4,24 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 let routes = [{
-    path: '/',
-    components: require('./components/member.vue')
+  path: '/',
+  components: require('./components/member.vue')
+}, {
+  path: '/address',
+  components: require('./components/address.vue'),
+  children:[{
+    path: '/all',
+    components:require('./components/all.vue')
+  }]
 }]
 
 // 2.创建router实例
 let router = new Router({
-    routes
-  })
+  routes
+})
 
 // 根组件的注入
 new Vue({
-    el: '#app',
-    router
-  })
+  el: '#app',
+  router
+})
